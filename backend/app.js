37ab39e8747266bfe671f4b2556e4525da2baa2b
Ivan Yurlov <ivanyurlov@yandex.ru>
2023-08-11 22:14:31 +0300

    "feat: ПР 15 - 1 редакция++"

--- backend/app.js
+++ backend/app.js
@@ -19,8 +19,12 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 4000 } = process.env;
 const app = express();
 
-// app.use(cors({ origin: 'http://localhost:3000' }));
-app.use(cors({ origin: 'https://ivanyurlov.nomoreparties.co' }));
+app.use(cors({
+  origin: [
+    'https://ivanyurlov.nomoreparties.co',
+    'http://ivanyurlov.nomoreparties.co',
+    'http://localhost:3000'],
+}));
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
