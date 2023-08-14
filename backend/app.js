7f85d2782d3a8ea5db0e345a270b2bad80c2d524
Ivan Yurlov <ivanyurlov@yandex.ru>
2023-08-14 07:25:00 +0300

    "fix: Удалить проверку на краш-тест"

--- backend/app.js
+++ backend/app.js
@@ -32,11 +32,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 
 app.use(express.json());
 app.use(requestLogger);
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().required(),
